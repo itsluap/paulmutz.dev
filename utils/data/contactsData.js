2770a8a923a4106d5953c7c05dc40b7c94f92f6b
itsluap <92261388+itsluap@users.noreply.github.com>
2024-12-01 15:01:53 -0600

    move back since its broken no matter what

---
+++ utils/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'paulmutzjr@icloud.com',
+    phone: '+1 262-613-1160',
+    address: 'Houston, TX',
+    github: 'https://github.com/',
+    linkedIn: 'https://www.linkedin.com/',
+}
