9b838c4eea911a676924f19d0df0b486aa6970c3
itsluap <92261388+itsluap@users.noreply.github.com>
2024-12-01 15:00:10 -0600

    -

--- utils/data/contactsData.js
+++
@@ -1,7 +0,0 @@
-export const contactsData = {
-    email: 'paulmutzjr@icloud.com',
-    phone: '+1 262-613-1160',
-    address: 'Houston, TX',
-    github: 'https://github.com/',
-    linkedIn: 'https://www.linkedin.com/',
-}
--- utils/data/educations.js
+++
@@ -1,8 +0,0 @@
-export const educations = [
-  {
-    id: 1,
-    title: "High School Diploma",
-    duration: "2018",
-    institution: "West Bend East High School",
-  }
-]
