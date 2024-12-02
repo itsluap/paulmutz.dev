ab8d9547f8c81ff2072f598595076208313e856e
itsluap <92261388+itsluap@users.noreply.github.com>
2024-12-01 21:21:38 -0600

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,62 +1,47 @@
-import ayla from '/public/image/ayla.jpg';
-import crefin from '/public/image/crefin.jpg';
-import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
+import fivem from '/public/image/fivem.jpg';
+import datingApp from '/public/image/dating-app.jpg';
+import blogSite from '/public/image/blog-site.jpg';
+import weatherApp from '/public/image/weather-app.jpg';
 
 export const projectsData = [
     {
         id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
+        name: 'Custom FiveM Server',
+        description: "I built a custom FiveM server for a gaming community using Lua. This project involved configuring game scripts, managing server-side optimizations, and implementing custom game modes. Alongside coding, I gained experience in deploying and maintaining both Windows and Linux servers for hosting the server. Additional tasks included database integration for player statistics and creating custom mods to enhance user experience.",
+        tools: ['Lua', 'Windows Server', 'Linux Server', 'MySQL', 'Git', 'SSH'],
+        role: 'Server Developer',
         code: '',
         demo: '',
-        image: crefin,
+        image: fivem,
     },
     {
         id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
+        name: 'Swipe Dating App',
+        description: "Developed a cross-platform dating app with functionality to match, chat, and swipe on profiles. Initially built with Flutter and MongoDB for real-time data handling, the app was later migrated to Firebase for enhanced scalability and simplicity in authentication and database management. The app included features such as user profiles, geolocation filtering, and media uploads.",
+        tools: ['Flutter', 'Firebase', 'Firestore', 'Dart', 'Cloud Functions'],
+        role: 'Mobile App Developer',
         code: '',
         demo: '',
-        image: travel,
+        image: datingApp,
     },
     {
         id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
+        name: 'Personal Blog Platform',
+        description: "Built a lightweight blog platform to showcase personal projects and share articles. The platform featured a custom-built API for blog post management, user authentication, and comments. The front-end was designed with React, while the back-end used Express and MongoDB. The project demonstrated skills in CRUD operations, RESTful APIs, and responsive design.",
+        tools: ['React', 'Express', 'MongoDB', 'Bootstrap', 'JWT'],
         role: 'Full Stack Developer',
+        code: '',
         demo: '',
-        image: realEstate,
+        image: blogSite,
     },
     {
         id: 4,
-        name: 'Newsroom Management',
-        description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
+        name: 'Weather Forecast App',
+        description: "Created a simple yet functional weather app that displays current and future weather data based on the user's location or search input. Leveraged OpenWeather API for accurate forecasts and used React and TailwindCSS to create a user-friendly interface. The app included features like temperature toggles, a weather icon display, and a search history.",
+        tools: ['React', 'OpenWeather API', 'TailwindCSS', 'Netlify'],
+        role: 'Front-End Developer',
         code: '',
         demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
+        image: weatherApp,
     }
 ];
-
-
-// Do not remove any property.
-// Leave it blank instead as shown below
-
-// {
-//     id: 1,
-//     name: '',
-//     description: "",
-//     tools: [],
-//     role: '',
-//     code: '',
-//     demo: '',
-//     image: crefin,
-// },
